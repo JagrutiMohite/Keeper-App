e39f5256b48d9c4cc88c761fc20db2c077eb39d1
Jagruti <mohitejagruti5@gmail.com>
2020-04-03 15:51:04 -0500

    Set up for keeper app using react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <footer>
+        <p>
+            copyright @ Jagruti Mohite
+        </p>
+        </footer>
+    )
+}
+
+export default Footer;
