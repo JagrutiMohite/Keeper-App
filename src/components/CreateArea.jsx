b5a08b68348e9740423de67b01e122f3c5de8510
Jagruti <mohitejagruti5@gmail.com>
2020-04-10 11:31:09 -0500

    styling app

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+    const [isExpanded, setExpanded] = useState(false);
     const [inputText, setInputText] = useState({
         title: "",
         content: ""
@@ -24,12 +28,17 @@ function CreateArea(props) {
         });
         event.preventDefault();
     }
+    function expand(){
+        setExpanded(true)
+    }
   return (
     <div>
-      <form >
-        <input onChange={handleChange} name="title" placeholder="Title" value={inputText.title}/>
-        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={inputText.content} />
-        <button onClick={submitNote}>Add</button>
+      <form className="create-note">
+      {isExpanded && <input onChange={handleChange} name="title" placeholder="Title" value={inputText.title}/>}
+        <textarea onClick={expand} onChange={handleChange} name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} value={inputText.content} />
+        <Zoom in={isExpanded}>
+        <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
 
 function Header(){
     return (
         <header>
             <h1>
-                Keeper App
+            <HighlightIcon />
+                Keeper
             </h1>
         </header>
     )
